6ad4b29fddfc8073e379bf623319fe5e3b778814
Kelly Siu <ksiu@LABA.PSWAS.LOCAL>
2023-11-01 15:54:25 -0700

    added backend and tailwindcss

---
+++ backend/config/db.js
@@ -0,0 +1,17 @@
+// connect mongoose to DB
+
+const mongoose = require('mongoose');
+
+let connectionString = `mongodb+srv://God:${process.env.MONGO_PASS}@cluster0.exyeecp.mongodb.net/MOD3?retryWrites=true&w=majority`
+
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+
+// log when connected
+
+mongoose.connection.once('open', ()=> {
+    console.log('connected to DATABASE');
+});
---
+++ backend/server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+require('dotenv').config();
+require('./config/db.js');
+const Transaction = require('./models/Transaction.js');
+const PORT = 3000;
+
+const app = express();
+
+
+// start middleware //
+app.use(express.json());
+app.use(cors({
+    origin: "*"
+}));
+app.use(morgan('dev'));
+app.use(helmet());
+// end //
+
+// start routes
+
+// get events
+
+// app.get("/events", async (req, res) => {
+//     let arrayOfEvents = await Event.find();
+//     res.send(arrayOfEvents);
+// })
+// app.delete("/events/:idOfEvent", async (req, res) => {
+//     let id = req.params.idOfEvent;
+//     let response = await Event.findByIdAndDelete(id);
+//     console.log(response);
+//     res.send('deleted event');
+// })
+
+// app.put('/events/:idOfEvent/', async (req,res) => {
+//     let id = req.params.idOfEvent;
+//     let updatedData = req.body;
+//     let response = await Event.findByIdAndUpdate(id, req.body, { new: true })
+//     console.log(response);
+//     res.send(response)
+// })
+
+// app.post("/events", async (req,res) => {
+//     // 1. get the data that was sent from the frontend
+//     // let eventData = req.body.eventData;
+//     // 2. Model.create(eventData)
+//     try {
+//         let response = await Event.create(req.body);
+//         res.status(201).send("created a new event!")
+//     } catch (err) {
+//         console.error(err)
+//         res.send("ERROR")
+//     }
+// })
+
+// // end routes
+
+app.listen(PORT, () => {
+    console.log(`server LIVE on port ${PORT}`);
+});
