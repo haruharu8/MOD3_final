f11231d869041c5dda9b323c834aed864ad683c1
Kelly Siu <senpai@Kellys-MacBook-Pro-2.local>
2023-11-07 14:35:31 -0800

    fixed AppContext not passing stuff right

--- client/src/context/appContext.jsx
+++ client/src/context/appContext.jsx
@@ -1,48 +1,62 @@
-import { createContext, useReducer } from "react";
+import React, { createContext, useReducer } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
-const AppReducer = (state, action) => {
-    switch(action.type){
-        case 'ADD_EXPENSE':
-            return {
-                ...state,
-                expenses: [...state.expenses, action.payload]
-            };
-        case 'DELETE_EXPENSE':
-            return {
-                ...state,
-                expenses: state.expenses.filter((expense) => expense.id !== action.payload ),
-            }
-        default:
-            return state;
-    }
-}
+// 5. The reduceer - this is used to update the state, based on the action
+export const AppReducer = (state, action) => {
+	switch (action.type) {
+		case 'ADD_EXPENSE':
+			return {
+				...state,
+				expenses: [...state.expenses, action.payload],
+			};
+		case 'DELETE_EXPENSE':
+			return {
+				...state,
+				expenses: state.expenses.filter(
+					(expense) => expense.id !== action.payload
+				),
+			};
+		case 'SET_BUDGET':
+			return {
+				...state,
+				budget: action.payload,
+			};
 
+		default:
+			return state;
+	}
+};
+
+// 1. Sets the initial state when the app loads
 const initialState = {
-    budget: 2000,
-    expenses: [
-        {   id: 12,
-            name: 'shopping',
-            cost: 40
-        },
-        {   id: 13,
-            name: 'holiday',
-            cost: 400
-        },
-    ]
-}
+	budget: 2000,
+	expenses: [
+		{ id: uuidv4(), name: 'Fruits', cost: 50 },
+		{ id: uuidv4(), name: 'Chanel bag', cost: 300 },
+		{ id: uuidv4(), name: 'Chocolate', cost: 70 },
+		{ id: uuidv4(), name: 'Rice', cost: 40 },
+		{ id: uuidv4(), name: 'Rizz', cost: 500 },
+	],
+};
 
+// 2. Context allows the props to use stuff
 export const AppContext = createContext();
 
+// 3. Provider gives components access to state
 export const AppProvider = (props) => {
-    const [state, dispatch] = useReducer(AppReducer, initialState);
+	// 4. Sets up the app state. takes a reducer, and an initial state
+	const [state, dispatch] = useReducer(AppReducer, initialState);
 
-    return (
-        <AppContext.Provider value={{
-            budget: state.budget,
-            expenses: state.expenses,
-            dispatch,
-        }}>
-            {props.children}
-        </AppContext.Provider>
-    );
+	// 5. Returns our context
+	return (
+		<AppContext.Provider
+			value={{
+				expenses: state.expenses,
+				budget: state.budget,
+				dispatch,
+			}}
+		>
+			{props.children}
+		</AppContext.Provider>
+	);
 };
